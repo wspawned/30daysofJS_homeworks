f60f8660a4a0b7290c294c345bf899f3468d8e8d
wspawned <80424496+wspawned@users.noreply.github.com>
2022-06-19 01:34:51 +0300

    homework2

--- index.js
+++ index.js
@@ -1,27 +1,58 @@
-//comments can make code readable
+let challenge = '30 days of JavaScript'
+console.log(challenge.length)
+console.log(challenge.toUpperCase())
+console.log(challenge.toLowerCase())
+console.log(challenge.substring(3, 7))
+console.log(challenge.substring(3, challenge.length-1))
+console.log(challenge.includes('Script'))
+console.log(challenge.split(' '))
+console.log('Facebook, Google, Microsoft, Apple, IBM, Oracle, Amazon'.split(', '))
+console.log(challenge.replace('JavaScript', 'Python'))
+console.log(challenge.charAt(15))
+console.log(challenge.indexOf('J'))
+console.log(challenge.charCodeAt(11))
+console.log(challenge.indexOf('a'), challenge.lastIndexOf('a'))
 
-//Welcome to 30DaysOfJavaScript
+let because = 'You cannot end a sentence with because because because is a conjunction'
+console.log(because.indexOf('because'), because.lastIndexOf('because'))
+console.log(because.search('because'))
 
-/* comments can make code readable
-    easy to reuse and informative
-    MULTILINECOMMENT
-*/
+let needTrim = '     30 days of JavaScript       '
+console.log(needTrim.trim())
 
-const firstName = 'irşad'
-const age = 27
-const aboutToDoSmt = true
-const cashFlow = null
-const interests = undefined
+console.log(challenge.startsWith('3'))
+console.log(challenge.endsWith('Script'))
+console.log(challenge.match('a'))
 
-console.log(typeof firstName)
-console.log(typeof age)
-console.log(typeof aboutToDoSmt)
-console.log(typeof cashFlow)
-console.log(typeof interests)
+let half = '30 days of'
+console.log(half.concat(' ', 'Javascript'))
 
-var ahmet 
-let mehmet = ('mehmet')
-console.log(ahmet, mehmet)
+console.log(challenge.repeat(2))
 
-let job= 'civil engineer', country= 'çanakkale'
-console.log(job, country)
+//level2
+
+let a = '10'
+console.log(typeof a == typeof 10)
+let b = Number(a)
+console.log(typeof b == typeof 10)
+
+let c = parseFloat('9.8')
+console.log(c == 10)
+let d = Math.round(c)
+console.log(d == 10)
+
+console.log('python'.search('on'), 'jargon'.search('on'))
+
+let jargon = 'I hope this course is not full of jargon.'
+console.log(jargon.search('jargon'))
+
+console.log(Math.floor(Math.random()*101))                  // btw 0-100
+console.log(Math.floor(50 + Math.random()*(51)))            // btw 50-100
+console.log(Math.floor(Math.random()*256))                  // btw 0-255
+
+
+
+const firstIndex = because.indexOf('b')
+const lastIndex = because.lastIndexOf('e')
+console.log(because.substr(firstIndex,lastIndex - firstIndex + 1))
+
